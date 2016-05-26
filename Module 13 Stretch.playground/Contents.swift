27169fe6bf0f9d6ca8139b2d9e4506e8988fb645
hmiranda3 <habibmiranda@gmail.com>
2016-05-26 09:33:00 -0600

    Stretch Commit

---
+++ Module 13 Stretch.playground/Contents.swift
@@ -0,0 +1,29 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+func numberFunc(number: [Int]) {
+    let printStatement = " "
+    for value in number {
+        if value > 50 {
+            print(printStatement + "Low")
+        } else {
+            print(printStatement + "high")
+        }
+        if value % 13 == 0 {
+            print(printStatement + "Didn't learn this in school!")
+        } else {
+            print(printStatement + "That's easy!")
+        }
+        
+        if (index, value)
+        
+    }
+}
+
+numberFunc([1, 45, 55, 1000, 2, 5, 99, 50, 62, 10])
+
